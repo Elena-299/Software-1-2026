7627c0fd5cb3577617b70f116fe86830006643b4
Elena <elenave@metropolia.fi>
2026-01-31 18:16:12 +0200

    31.1.2026

--- Module 6/6.6 - Better pizza value.py
+++ Module 6/6.6 - Better pizza value.py
@@ -8,12 +8,12 @@ def unit_pizza(diameter, price):
 diameter1 = float(input("Enter the diameter of the first pizza in centimeters: "))
 price1 = float(input("Enter the price of the first pizza in euros: "))
 unit1 = unit_pizza(diameter1, price1)
-print(f"{unit1:4.3}")
+#print(f"{unit1:4.3}")
 
 diameter2 = float(input("Enter the diameter of the second pizza in centimeters: "))
 price2 = float(input("Enter the price of the second pizza in euros: "))
 unit2 = unit_pizza(diameter2, price2)
-print(f"{unit2:4.3}")
+#print(f"{unit2:4.3}")
 
 if unit1 > unit2:
     print("The second pizza provides better value for its price.")
